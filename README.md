7be39cc78c0f68ea259e90cf50cc186a1dece9b7
oliverhegardt <90907842+oliverhegardt@users.noreply.github.com>
2021-09-30 11:26:17 +0200

    commit 30/9 1126

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Github-minblogg1
+
+Welcome to my blog. My blog is a swimming blog. The blog retrieves data from a firestore database.
--- public/js/home.js
+++ public/js/home.js
@@ -1,21 +1,22 @@
+//hämta blogSection elementet med hjälp av DOM.
 const blogSection = document.querySelector(".blogs-section");
 
+//hämtar våra blogginlägg från vår databas som heter blogs, asynchronous call för att det kan ta tid att hämta inläggen så man fortfarande kan använda sidan. foreEach loop - för varje blogg i databasen skapa en blogg.
 db.collection("blogs")
   .get()
   .then((blogs) => {
     blogs.forEach((blog) => {
-      if (blog.id != decodeURI(location.pathname.split("/").pop())) {
-        createBlog(blog);
-      }
+      createBlog(blog);
     });
   });
 
+//får ut datan av varje element
 const createBlog = (blog) => {
   let data = blog.data();
   blogSection.innerHTML += `
     <div class="blog-card">
         <img src="${data.bannerImage}" class="blog-image" alt="">
-        <h1 class="blog-title">${data.title.substring(0, 100) + "..."}</h1>
+        <h1 class="blog-title">${data.title.substring(0, 100)}</h1>
         <p class="blog-overview">${data.article.substring(0, 200) + "..."}</p>
         <a href="/${blog.id}" class="btn dark">read</a>
     </div>
