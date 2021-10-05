5e81d3511dc4744431b69e4af0d6d2b715414de3
oliverhegardt <90907842+oliverhegardt@users.noreply.github.com>
2021-10-05 11:09:05 +0200

    commit 05/10 1108

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 //importera alla paket
 //importera express bibloteket, express är för servern
 const express = require("express");
+//gör så att vi kan jobba med filer och directory vägar
 const path = require("path");
 //importerar fileupload paketet
 const fileupload = require("express-fileupload");
 
-//skapar ett nytt directory(katalogväg) i server.js och namnet på den nya directoryn är public
+//skapar ett nytt directory(katalogväg) och namnet på den nya directoryn är public
 let initial_path = path.join(__dirname, "public");
 
-//ställa in servern, genom att ropa på express funktionen, skapar vi en applikation som gör det möjligt att starta vår server
+//ställa in servern, genom att ropa på express funktionen, skapar vi en applikation som gör det möjligt att starta vår server. express (); startar servern. vi skapar en express application
 const app = express();
-//här säger vi att node.js ska använda __dirname till att peka till public directory(filmappen) som innehåller statiska filer
 
+//här säger vi att node.js ska peka till public directory(filmappen) som innehåller statiska filer
 app.use(express.static(initial_path));
 app.use(fileupload());
 
@@ -54,7 +55,6 @@ app.post("/upload", (req, res) => {
   });
 });
 
-//jQuery get() Method
 app.get("/:blog", (req, res) => {
   res.sendFile(path.join(initial_path, "blog.html"));
 });
@@ -63,7 +63,7 @@ app.use((req, res) => {
   res.json("404");
 });
 
-//för att få vår server att fungera/köra, vår app(server) lyssnar på port 3000 efter massa begäran(uppgifter) den ska göra
+//för att få vår server att fungera/köra, vår app(server) lyssnar på port 3000 efter massa begäran(uppgifter) som den ska göra
 
 app.listen("3000", () => {
   console.log("listening......");
